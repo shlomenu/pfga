dcc322d66f2c4ec80b626af1af553695036bc1af
shlomenu <eliw55@gmail.com>
2019-12-13 18:55:40 -0500

    refactored, added scarcity parameters

--- src/Robbie.hs
+++ src/Robbie.hs
@@ -1,6 +1,5 @@
 module Robbie (
-  mkGrid,
-  mkSubGrid,
+  makeRW
 ) where
 
 import System.Random(randomRs, mkStdGen, split, StdGen, Random, RandomGen)
@@ -8,26 +7,26 @@ import Data.List(splitAt)
 import Data.Array.IArray(IArray, Array, Ix, array)
 import Data.Array.Unboxed(UArray)
 import Data.Word(Word8)
-
-{-mkSqGrid :: Int -> Int -> Array Int (UArray Int Word8)    
-mkSqGrid seed dim = runSTArray $ do
-    newArray(0, dim + 1) $ runSTUArray $ do 
-        newArray(0, dim + 1) 0
--}
-mkGrid :: (IArray a (b Int e), IArray b e, Num e, Random e, RandomGen g) =>
-          Int -> g 
-            -> a Int (b Int e)
-mkGrid n g = array (0,n+1) subGrid where
-    subGrid = zip [0..n+1] $ mkSubGrid n $ randomRs (1,2) g
-
-mkSubGrid :: (IArray a e, Num e) => Int -> [e] -> [a Int e]
-mkSubGrid n xs@(_:_) = map (mkArray n) $ replicate (n+2) 0 : mkMidRows n xs where
-    mkMidRows :: (Num a) => Int -> [a] -> [[a]]
-    mkMidRows n xs@(_:_) = gs : mkMidRows n rs where
-        gs = 0 : (g ++ [0])
-        (g, rs) = splitAt n xs
-    mkMidRows n [] = replicate (n+2) 0 : []
-    mkArray :: (IArray a e, Ix i, Num i, Enum i) => i -> [e] -> a i e
-    mkArray n = array (0,n+1) . zip [0..n+1]
+import Data.Ratio(approxRational, numerator, denominator)
 
 
+type RobbieWorld = Array Int (UArray Int Word8)
+
+makeRW :: (RandomGen g, RealFrac c) => Int -> c -> c -> g -> RobbieWorld
+makeRW n frac eps g = mkArray n $ map (mkArray n) $ mkSquareGrid n 0 rs where
+    rs = take (n*n) $ map (shift nm) $ randomRs (1, dnm) g
+    shift s x = if x > s then 1 else 2
+    nm = numerator $ approxRational frac eps
+    dnm = denominator $ approxRational frac eps
+
+mkSquareGrid :: (Num e) => Int -> e -> [e] -> [[e]]
+mkSquareGrid n endfix xs@(_:_) = replicate (n+2) endfix : mkElse n endfix xs 
+
+mkElse :: (Num e) => Int -> e -> [e] -> [[e]]
+mkElse n endfix xs@(_:_) = gs : mkElse n endfix rs where
+    gs = endfix : (g ++ [endfix])
+    (g, rs) = splitAt n xs
+mkElse n endfix []       = replicate (n+2) endfix : []
+
+mkArray :: (IArray a e, Ix i, Num i, Enum i) => i -> [e] -> a i e
+mkArray n = array (0,n+1) . zip [0..n+1]
