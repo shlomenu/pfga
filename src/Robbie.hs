2ab7726fd224fbd05a2d28bdd23cb7e327a12ca9
shlomenu <eliw55@gmail.com>
2019-12-13 17:57:20 -0500

    Got types working for first time with grid!

--- src/Robbie.hs
+++ src/Robbie.hs
@@ -1,11 +1,33 @@
-import System.Random(randomRs)
+module Robbie (
+  mkGrid,
+  mkSubGrid,
+) where
 
-mkSqGrid :: Int -> Int -> Array Int (UArray Int Word8)    
+import System.Random(randomRs, mkStdGen, split, StdGen, Random, RandomGen)
+import Data.List(splitAt)
+import Data.Array.IArray(IArray, Array, Ix, array)
+import Data.Array.Unboxed(UArray)
+import Data.Word(Word8)
+
+{-mkSqGrid :: Int -> Int -> Array Int (UArray Int Word8)    
 mkSqGrid seed dim = runSTArray $ do
     newArray(0, dim + 1) $ runSTUArray $ do 
         newArray(0, dim + 1) 0
-    
+-}
+mkGrid :: (IArray a (b Int e), IArray b e, Num e, Random e, RandomGen g) =>
+          Int -> g 
+            -> a Int (b Int e)
+mkGrid n g = array (0,n+1) subGrid where
+    subGrid = zip [0..n+1] $ mkSubGrid n $ randomRs (1,2) g
 
-
+mkSubGrid :: (IArray a e, Num e) => Int -> [e] -> [a Int e]
+mkSubGrid n xs@(_:_) = map (mkArray n) $ replicate (n+2) 0 : mkMidRows n xs where
+    mkMidRows :: (Num a) => Int -> [a] -> [[a]]
+    mkMidRows n xs@(_:_) = gs : mkMidRows n rs where
+        gs = 0 : (g ++ [0])
+        (g, rs) = splitAt n xs
+    mkMidRows n [] = replicate (n+2) 0 : []
+    mkArray :: (IArray a e, Ix i, Num i, Enum i) => i -> [e] -> a i e
+    mkArray n = array (0,n+1) . zip [0..n+1]
 
 
