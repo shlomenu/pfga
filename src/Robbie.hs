3a34b7e57fd554e539b8cf886222fd85dfdc6000
shlomenu <eliw55@gmail.com>
2019-12-15 21:35:59 -0500

    Finished core

--- src/Robbie.hs
+++ src/Robbie.hs
@@ -1,24 +1,71 @@
 module Robbie (
-  makeRW
+   Sim
+ , Label
+ , RobbieWorld
+ , RobbieState
+ , Genome 
+ , makeRW
+ , mkGenome
+ , mkRobbieState
+ , act
+ , crossGenomes
+ , mutateGenome
 ) where
 
-import System.Random(randomRs, randomR, getStdRandom, RandomGen)
+-----------------------------------------------------------------
+
+{- 
+      function imports: 
+-}
+
+import System.Random( randomRs, randomR, 
+                      getStdRandom, randomRIO, 
+                      newStdGen )
+import Data.Array.MArray( readArray, writeArray, 
+                          newArray_, newListArray )
+import Data.Ratio( approxRational, numerator, denominator )
+import Control.Monad( guard, forM_, forM )
+import Data.Maybe( catMaybes, fromJust )
+import Data.List( foldl' )
+
+{- 
+      type imports: 
+-}
+
+import System.Random(RandomGen)
 import Data.Array.IO(IOUArray, IOArray)
-import Data.Array.MArray(readArray, writeArray, newArray_, newListArray)
 import Data.Word(Word8)
-import Data.Ratio(approxRational, numerator, denominator)
-import Control.Monad(guard, forM_)
 import Data.IntMap.Strict(IntMap)
-import qualified Data.IntMap as IM
 import Data.Map(Map)
+
+{-
+      qualified imports:
+-}
+
+import qualified Data.IntMap as IM
 import qualified Data.Map as M
-import Data.Maybe(catMaybes)
+import qualified Data.IntSet as IS
+
+-----------------------------------------------------------------
+{- Constructors & Type Synonyms -}
 
 data Sim = Wrap RobbieWorld Genome RobbieState
+data Label = MvRand 
+           | North 
+           | South 
+           | East 
+           | West 
+           | Stay 
+           | Collect 
+  deriving (Eq, Ord)
+
 type Action = (Sim -> IO Sim)
 type RobbieWorld = IOArray Int (IOUArray Int Word8) 
-type Genome = IntMap Action 
+type Genome = IntMap Label 
 type RobbieState = IOUArray Word8 Int 
+
+-----------------------------------------------------------------
+{- Constants -}
 
 mistakePenalty :: Int
 mistakePenalty = 5
@@ -26,12 +73,80 @@ mistakePenalty = 5
 rewardCollect :: Int
 rewardCollect = 1
 
-act :: Sim -> IO Sim
+labels :: [Label]
+labels = [MvRand, North, South, East, West, Stay, Collect]
+
+actions :: [Action]
+actions = [mvRand, north, south, east, west, stay, collect]
+
+hashes :: [Int]
+hashes = do 
+  let vs = [0..2]
+  n <- vs
+  s <- vs
+  e <- vs
+  w <- vs
+  h <- vs
+  guard (h /= 0)
+  guard ((length $ filter (==0) [n, s, e, w, h]) < 3)
+  return $ hashLoc h s n e w
+
+actMap :: Map Label Action
+actMap = M.fromList $ zip labels actions
+        
+labelMap :: Map Int Label
+labelMap = M.fromList $ zip [1..7] labels
+
+rLabelMap :: Map Label Int
+rLabelMap = M.fromList $ zip labels [1..7]
+
+-----------------------------------------------------------------
+{- Evolution -}
+
+mutateGenome :: (RealFrac c) => c -> c -> Genome -> IO Genome
+mutateGenome frac eps gnm = do
+    let size = IM.size gnm
+        rt = approxRational frac eps
+        (nm, dnm) = (numerator rt, denominator rt) 
+    
+    rs <- forM [1..size] $ \_ -> randomRIO (1,dnm)
+    g <- newStdGen
+    
+    let keys = map snd $ filter dropRatio $ zip rs $ IM.keys gnm 
+        dropRatio (r,_) = if r > nm then False else True
+        vs = noNothingLkup rLabelMap M.lookup $ noNothingLkup gnm IM.lookup keys
+        mutns = mapMutants vs $ randomRs (1,7) g
+        newIMap = foldl' mdf gnm $ zip keys $ noNothingLkup labelMap M.lookup mutns
+    
+    return newIMap
+
+mapMutants :: (Eq a) => [a] -> [a] -> [a]
+mapMutants e@(b:bs) (c:cs) | c /= b    = c : mapMutants bs cs
+                           | otherwise = mapMutants e cs
+mapMutants [] _ = []
+mapMutants _ [] = []
+
+mdf :: Genome -> (Int, Label) -> Genome
+mdf gnm (k,a) = IM.update (\_ -> Just a) k gnm
+
+crossGenomes :: Genome -> Genome -> IO (Genome, Genome)
+crossGenomes gnmA gnmB = do 
+    let n = IM.size gnmA
+    r <- randomRIO (1, n - 1)
+    let part = IS.fromAscList $ take r $ IM.keys gnmA
+        (btmA, topA) = IM.partitionWithKey (\k _ -> IS.member k part) gnmA
+        (btmB, topB) = IM.partitionWithKey (\k _ -> IS.member k part) gnmB
+    return (btmA `IM.union` topB, btmB `IM.union` topA)
+
+-----------------------------------------------------------------
+{- Stepping Simulations Forward -}
+
+act :: Action
 act sim@(Wrap rw gnm rs) = do
     sur <- sense rw rs 
-    case IM.lookup sur gnm of
-        (Just step) -> step sim
-        Nothing     -> error "act: state not found in genome: this should not ever occur"
+    let step = fromJust $ M.lookup lbl actMap
+        lbl = fromJust $ IM.lookup sur gnm
+    step sim
       
 sense :: RobbieWorld -> RobbieState -> IO Int
 sense rw rs = do 
@@ -48,14 +163,14 @@ access2d (x, y) rw = do
     row <- readArray rw x
     readArray row y
 
-mvRand :: Sim -> IO Sim
+mvRand :: Action
 mvRand sim@(Wrap _ gnm _) = do
     r <- getStdRandom $ randomR (2,5) 
-    case IM.lookup r gnm of
-        (Just go) -> go sim
-        Nothing   -> error "mvRand: received value not in genome: absolutely should not happen"
+    let go = fromJust $ M.lookup lbl actMap
+        lbl = fromJust $ IM.lookup r gnm
+    go sim
       
-collect :: Sim -> IO Sim
+collect :: Action
 collect sim@(Wrap rw gnm rs) = do
     x <- readArray rs 1
     y <- readArray rs 2
@@ -69,19 +184,19 @@ collect sim@(Wrap rw gnm rs) = do
         writeArray rs 0 $ score + rewardCollect
         return $ Wrap rw gnm rs
 
-stay :: Sim -> IO Sim
+stay :: Action
 stay sim = return sim
 
-north :: Sim -> IO Sim
+north :: Action
 north = move (1,0)
       
-south :: Sim -> IO Sim
+south :: Action
 south = move (-1,0)
  
-east :: Sim -> IO Sim 
+east :: Action
 east = move (0,1)
      
-west :: Sim -> IO Sim
+west :: Action
 west = move (0,-1)
 
 move :: (Int, Int) -> Sim -> IO Sim
@@ -102,31 +217,18 @@ fetchRel (i, j) rs rw = do
     y <- readArray rs 2
     access2d (x + i, y + j) rw
 
+-----------------------------------------------------------------
+{- "Constructors" -}
+
 mkRobbieState :: (RandomGen g) => Int -> g -> IO RobbieState 
 mkRobbieState n g = newListArray (0,2) es where
     es = 0 : (rs ++ [0])
     rs = take 2 $ randomRs (1,n) g
 
 mkGenome :: (RandomGen g) => g -> Genome
-mkGenome g = IM.fromList $ flip zip actions $ do 
-      let vs = [0..2]
-      n <- vs
-      s <- vs
-      e <- vs
-      w <- vs
-      h <- vs
-      guard (h /= 0)
-      guard ((length $ filter (==0) [n, s, e, w, h]) < 3)
-      return $ hashLoc h s n e w
-    where actions = catMaybes $ map (flip M.lookup actMap) $ randomRs (1,7) g
-        
-actMap :: Map Word8 Action
-actMap = M.fromList $ zip [1..7] [mvRand, north, south, east, west, stay, collect]
-
-hashLoc :: Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Int
-hashLoc h n s e w = sum $ zipWith (*) integralLoc powersOf3 where
-    integralLoc = map fromIntegral [h,n,s,e,w]
-    powersOf3 = iterate (3*) 1
+mkGenome g = IM.fromList $ zip hashes lbls where
+  lbls = noNothingLkup labelMap M.lookup rs
+  rs = randomRs (1,7) g
 
 makeRW :: (RealFrac c) => Int -> c -> c -> IO RobbieWorld
 makeRW n frac eps = do 
@@ -148,4 +250,26 @@ makeRW n frac eps = do
     shift s x = if x > s then 1 else 2
     nm  = numerator $ approxRational frac eps
     dnm = denominator $ approxRational frac eps
+
+-----------------------------------------------------------------
+{- Utilities & Abbreviations -}
+
+noNothingLkup :: b -> (a -> b -> Maybe c) -> [a] -> [c]
+noNothingLkup m lkup ks = catMaybes $ map (flip lkup m) ks
+
+hashLoc :: Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Int
+hashLoc h n s e w = sum $ zipWith (*) integralLoc powersOf3 where
+    integralLoc = map fromIntegral [h,n,s,e,w]
+    powersOf3 = iterate (3*) 1
+
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+                    {-     Fin      -}
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+
+
+
 
